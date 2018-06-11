5e063fd13d6cb6164e7d476300765f1337e7dcf6
Alexis Villegas Torres <alexisvt@gmail.com>
2018-06-11 13:02:06 -0600

    finished sample

--- use-external-packages/bin/main.dart
+++ use-external-packages/bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
 
 main(List<String> arguments) {
   var url = 'http://www.voidrealms.com';
@@ -7,4 +8,6 @@ main(List<String> arguments) {
     print('Response status code: ${response.statusCode}');
     print('Response body: ${response.body.substring(0, 150)}');
   });
+
+  print('path joined: ${path.join('lib', 'use_external_packages.dart')}');
 }
